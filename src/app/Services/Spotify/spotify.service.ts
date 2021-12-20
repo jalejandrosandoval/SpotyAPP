c9a3734cb67f681514d0888217a2e2690208de6b
jalejandrosandoval <jsandoval5@udi.edu.co>
2021-12-20 08:52:49 -0500

    Add new Methods.

--- src/app/Services/Spotify/spotify.service.ts
+++ src/app/Services/Spotify/spotify.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { IAlbums, IArtist } from 'src/app/Interfaces/Albums.interface';
+import { ITopTracks } from 'src/app/Interfaces/TopTracks.interfaces';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -11,19 +13,29 @@ export class SpotifyService {
   constructor(private _httpClient: HttpClient) { }
 
   // Method for get the Query
-  private getQuery(_Query: string){
+  private getQuery(_Query: string, _method: string = 'get'): Observable<any>{
     let _url = `${environment.Spotify.URL.API_URL_Spotify}${_Query}`;
-    return this._httpClient.get(_url);
+    return this._httpClient.request(_method,_url);
   }
 
   // Method for the releases
-  public getNewReleases(){
+  public getNewReleases(): Observable<IAlbums[]>{
     return this.getQuery('browse/new-releases?limit=20')
       .pipe(
         map(
           (data: any) => data['albums'].items
         )
       );
+  }
+
+  // Method fot get the Artists
+  public getArtist(_Id: string): Observable<IArtist[]>{
+    return this.getQuery(`artists/${_Id}`);
+  }
+
+  // Method fot get the TopTracks
+  public getTopTracks(_Id: string, _CountryCode: string = 'us'): Observable<ITopTracks[]>{
+    return this.getQuery(`artists/${_Id}/top-tracks?country=${_CountryCode}`);
   }
 
 }
